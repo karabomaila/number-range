2ae4101d0b3fc98a71cd6a7477fa9caf77903ed8
Karabo <mailakarabo3@gmail.com>
2025-11-02 13:45:54 +0200

    feat: to summarize range of numbers
    Implement methods to process a user input and summarize the input numbers. Add tests to test the methods.

---
+++ src/numberrangesummarizer/NumberRangeSummarizerImp.java
@@ -0,0 +1,47 @@
+package src.numberrangesummarizer;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class NumberRangeSummarizerImp {
+    public Collection<Integer> collect(String input){
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+        for (String num: input.split(",")) nums.add(Integer.parseInt(num.trim()));
+        return nums;
+    }
+
+    public String summarizeCollection(Collection<Integer> input){
+        if (input.size() == 0) return "";
+        List<String> result = new ArrayList<String>();
+
+        int prev_num = 0;
+        int i = 0;
+        for (int curr_num: input){
+            if (i == 0){
+                result.add(Integer.toString(curr_num));
+            }
+            else{
+                String curr_sq = result.get(result.size() - 1);
+                
+                if (prev_num == curr_num - 1 && i == input.size() - 1){
+                    result.set( result.size() - 1, curr_sq + "-" + Integer.toString(curr_num));          
+                }
+                else if (prev_num != curr_num - 1){
+                    String prev_num2 = Integer.toString(prev_num);
+
+                    if (!curr_sq.equals(prev_num2)){
+                        result.set( result.size() - 1, curr_sq + "-" + prev_num2);                   
+                    }
+
+                    result.add(Integer.toString(curr_num));
+                }
+            }
+
+            prev_num = curr_num;
+            i++;
+        }
+        
+        return result.toString().substring(1, result.toString().length() - 1);
+        
+    }
+}
---
+++ src/test/NumberRangeSummarizerImpTest.java
@@ -0,0 +1,72 @@
+package src.test;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+import src.numberrangesummarizer.NumberRangeSummarizerImp;
+
+import java.util.Collection;
+import java.util.Arrays;
+
+
+
+public class NumberRangeSummarizerImpTest{
+    NumberRangeSummarizerImp numberRangeSummarizerImp = new NumberRangeSummarizerImp();
+
+    @Test
+    public void testCollect() {
+        String input = "1, 3 ,6,7,8, 12,13,14";
+        Collection<Integer> expected = Arrays.asList(1, 3, 6, 7, 8, 12, 13, 14);
+        assertEquals(expected, numberRangeSummarizerImp.collect(input));
+    }
+
+    @Test 
+    public void testCollectWithEmptyInput() {
+        String input = "";
+        Collection<Integer> expected = Arrays.asList();
+        assertEquals(expected, numberRangeSummarizerImp.collect(input));
+    }
+
+    @Test
+    public void testSummarizeCollection() {
+        Collection<Integer> input = Arrays.asList(1, 3, 6, 7, 8, 12, 13, 14, 15, 21, 22, 23, 24, 31);
+        String expected = "1, 3, 6-8, 12-15, 21-24, 31";
+        assertEquals(expected, numberRangeSummarizerImp.summarizeCollection(input));
+    }
+
+    @Test
+    public void testSummarizeCollectionWithEmptyInput() {
+        Collection<Integer> input = Arrays.asList();
+        String expected = "";
+        assertEquals(expected, numberRangeSummarizerImp.summarizeCollection(input));
+    }
+
+    @Test
+    public void testSummarizeCollectionWithSingleNumber() {
+        Collection<Integer> input = Arrays.asList(5);
+        String expected = "5";
+        assertEquals(expected, numberRangeSummarizerImp.summarizeCollection(input));
+    }
+
+    @Test
+    public void testSummarizeCollectionWithAllSequentialNumbers() {
+        Collection<Integer> input = Arrays.asList(1, 2, 3, 4, 5);
+        String expected = "1-5";
+        assertEquals(expected, numberRangeSummarizerImp.summarizeCollection(input));
+    }
+
+    @Test
+    public void testSummarizeCollectionWithNoSequentialNumbers() {
+        Collection<Integer> input = Arrays.asList(1, 2, 4);
+        String expected = "1, 2, 4";
+        assertEquals(expected, numberRangeSummarizerImp.summarizeCollection(input));
+    }
+
+    @Test 
+    public void testSummarizeCollectionWithDuplicateNumbers() {
+        Collection<Integer> input = Arrays.asList(1,2,3,3,5,6,1,2,3,6,6,32);
+        String expected = "1-3, 3, 5-6, 1-3, 6, 6, 32";
+
+        assertEquals(expected, numberRangeSummarizerImp.summarizeCollection(input));
+    }
+    
+}
